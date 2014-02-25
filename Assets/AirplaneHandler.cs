28bdb24f40fc7d44251361dd24a60177e5cbe143
Kyle Dold <kdold@openboxsoftware.com>
2014-02-25 15:42:25 +0200

    Updates to NY model, includes airplane

---
+++ Assets/AirplaneHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirplaneHandler : MonoBehaviour {
+
+	public bool playAnimation = true;
+
+	void Update () {
+		if (this.playAnimation) {
+			iTween.MoveTo(this.gameObject, iTween.Hash(
+				"path", iTweenPath.GetPath("FlightPath"),
+				//"orienttopath", true,
+				"time", 10,
+				"lookTime", 0.2,
+				"easetype", iTween.EaseType.linear,
+      			"looptype", iTween.LoopType.loop));
+			playAnimation = false;
+		}
+	}
+}
