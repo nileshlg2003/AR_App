95a1008d3bb68a95cb9a2a62c87963b5ebb457b0
Logan <logan.dunbar@gmail.com>
2014-03-03 16:13:10 +0200

    Added a GUIState file to handle transitions between scenes

---
+++ Assets/Scripts/MainGUI/GUIState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIState
+{
+		public enum ModelType
+		{
+				NewYorkSkyline,
+				Vegas
+		}
+
+		public static ModelType mSelectedModelType { get; set; }
+}
